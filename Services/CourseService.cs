fcb8c342c0749fafe0762a1707924789f9e07afc
HagerMagdy2 <hager9671@gmail.com>
2026-02-24 01:29:05 +0200

    Add Service Layer

---
+++ Services/CourseService.cs
@@ -0,0 +1,29 @@
+﻿using Examination_System.Models;
+using Examination_System.Repositories;
+
+namespace Examination_System.Services
+{
+    public class CourseService
+    {
+        GeneralRepository<Course> _generalRepository;
+        public CourseService()
+        {
+            _generalRepository = new GeneralRepository<Course>();
+        }
+        public IQueryable<Course> GetAll()
+        {
+            //Add Validation here if needed
+            return _generalRepository.GetAll();
+        }
+        public async Task<Course> GetById(int id)
+        {
+            return await _generalRepository.GetById(id);
+        }
+     
+        public async Task<bool> DeleteCourse(int id)
+        {
+            _generalRepository.DeleteCourse(id);
+            return true;
+        }
+    }
+}
