94f33407155850e571092570789fb3341c7b4c1a
HagerMagdy2 <hager9671@gmail.com>
2026-02-25 23:42:46 +0200

    Mapping

--- DTOs/Course/CourseProfile.cs
+++ DTOs/Course/CourseProfile.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using Examination_System.DTOs.Instructor;
+using Examination_System.Migrations;
 using Examination_System.Models;
 using Examination_System.ViewModels.Course;
 using Examination_System.ViewModels.Instructor;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
 
 
 namespace Examination_System.DTOs.Course
@@ -17,8 +21,8 @@ namespace Examination_System.DTOs.Course
             CreateMap<GetAllCoursesViewModel, GetAllCoursesDTO>().ReverseMap();
             CreateMap<GetInstructorInfoViewModel, GetInstructorInfoDTO>().ReverseMap();
             CreateMap<Examination_System.Models.Instructor, GetInstructorInfoDTO>()
-                .ForMember(des=>des.Name,opt=>opt.MapFrom(src => src.FullName))
-                .ReverseMap();
+            .ForMember(des=>des.Name,opt=>opt.MapFrom(src => src.FullName))
+            .ReverseMap();
         }
           
     }
